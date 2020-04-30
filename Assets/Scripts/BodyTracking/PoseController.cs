45c4d42d0b09206842d6040980c60bec2a49c222
Kevin Oré <kevinorh@hotmail.com>
2020-04-29 19:07:55 -0500

    Identificar posisiones Resume y Pause terminado

---
+++ Assets/Scripts/BodyTracking/PoseController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PoseController : MonoBehaviour
+{
+    //Posicion en función a la diferencia entre el root y el joint
+    public string ResumePosition(JointTracker[] joints)
+    {
+        JointTracker leftHandJoint = joints.Where(x => x.gameObject.transform.parent.name == "LeftHand").SingleOrDefault();
+        JointTracker rigthHandJoint = joints.Where(x => x.gameObject.transform.parent.name == "RightHand").SingleOrDefault();
+        JointTracker rootJoint = joints.Where(x => x.gameObject.transform.parent.name == "Root").SingleOrDefault();
+
+        if (leftHandJoint == null || rigthHandJoint == null || rootJoint == null)
+            return "false";
+        else
+        {
+            var response = "";
+            response += $"Right(x)-Root(x) (-0.3 y 0): {rigthHandJoint.transform.position.x - rootJoint.transform.position.x}\n";
+            response += $"Left(x)-Root(x) (0 y 0.3): {leftHandJoint.transform.position.x - rootJoint.transform.position.x}\n";
+            response += $"Right(x)-Left(x) (-0.3 y 0): {rigthHandJoint.transform.position.x - leftHandJoint.transform.position.x}\n";
+            response += $"Left(y)-Right(y) (-0.1 y 0.1): {leftHandJoint.transform.position.y - rigthHandJoint.transform.position.y}\n";
+            response += $"Left(y)-Root(y) (>=0.8): {leftHandJoint.transform.position.y - rootJoint.transform.position.y}\n";
+            
+            if (rigthHandJoint.transform.position.x - rootJoint.transform.position.x <= 0 &&
+                rigthHandJoint.transform.position.x - rootJoint.transform.position.x >= -0.3 &&
+                leftHandJoint.transform.position.x - rootJoint.transform.position.x >= 0 &&
+                leftHandJoint.transform.position.x - rootJoint.transform.position.x <= 0.3 &&
+                rigthHandJoint.transform.position.x - leftHandJoint.transform.position.x >= -0.3 &&
+                rigthHandJoint.transform.position.x - leftHandJoint.transform.position.x <= 0 &&
+                leftHandJoint.transform.position.y - rigthHandJoint.transform.position.y >= -0.1 &&
+                leftHandJoint.transform.position.y - rigthHandJoint.transform.position.y <= 0.1 &&
+                leftHandJoint.transform.position.y - rootJoint.transform.position.y >= 0.8)
+                return response += "true";
+            else
+                return response+="false";             
+        }
+    }
+    public bool PausePosition(JointTracker[] joints)
+    {
+        JointTracker leftHandJoint = joints.Where(x => x.gameObject.transform.parent.name == "LeftHand").SingleOrDefault();
+        JointTracker rigthHandJoint = joints.Where(x => x.gameObject.transform.parent.name == "RightHand").SingleOrDefault();
+        JointTracker rootJoint = joints.Where(x => x.gameObject.transform.parent.name == "Root").SingleOrDefault();
+
+        if (leftHandJoint == null || rigthHandJoint == null || rootJoint == null)
+            return false;
+        else
+        {
+            
+            if (rigthHandJoint.transform.position.x - rootJoint.transform.position.x <= -0.3 &&
+                rigthHandJoint.transform.position.x - rootJoint.transform.position.x >= -0.5 &&
+                leftHandJoint.transform.position.x - rootJoint.transform.position.x >= 0.3 &&
+                leftHandJoint.transform.position.x - rootJoint.transform.position.x <= 0.5 &&
+                rigthHandJoint.transform.position.x - leftHandJoint.transform.position.x >= -1.0 &&
+                rigthHandJoint.transform.position.x - leftHandJoint.transform.position.x < -0.3 &&
+                leftHandJoint.transform.position.y - rigthHandJoint.transform.position.y >= -0.1 &&
+                leftHandJoint.transform.position.y - rigthHandJoint.transform.position.y <= 0.1 &&
+                leftHandJoint.transform.position.y - rootJoint.transform.position.y >= 0.8)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- Assets/Scripts/HumanBodyTracking.cs
+++ Assets/Scripts/HumanBodyTracking.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -22,10 +24,27 @@ public class HumanBodyTracking : MonoBehaviour
     [SerializeField]
     private float skeletonOffsetZ = 0;
 
+    [SerializeField]
+    private Text SpeedRigth;
+
+    [SerializeField]
+    private Text PositionRigth;
+
+    [SerializeField]
+    private Text SpeedLeft;
+
+    [SerializeField]
+    private Text PositionLeft;
 
     [SerializeField] private ARHumanBodyManager humanBodyManager;
 
     private Dictionary<TrackableId, HumanBoneController> skeletonTracker = new Dictionary<TrackableId, HumanBoneController>();
+
+    private JointTracker[] jointTrackers;
+
+    private PoseController poseController = new PoseController();
+
+    private double TimeCounter = 0; 
 
     public ARHumanBodyManager HumanBodyManagers
     {
@@ -41,7 +60,8 @@ public class HumanBodyTracking : MonoBehaviour
 
     void OnEnable()
     {
-        Debug.Assert(humanBodyManager != null, "Human body manager is required.");
+        TimeCounter = 0;
+        UnityEngine.Debug.Assert(humanBodyManager != null, "Human body manager is required.");
         humanBodyManager.humanBodiesChanged += OnHumanBodiesChanged;
     }
 
@@ -52,14 +72,33 @@ public class HumanBodyTracking : MonoBehaviour
     }
     void OnHumanBodiesChanged(ARHumanBodiesChangedEventArgs eventArgs)
     {
+        //To get time of execution
+        DateTime start = DateTime.Now;
+
+        SpeedRigth.text = string.Empty;
+        SpeedLeft.text = string.Empty;
+
         HumanBoneController humanBoneController;
 
         foreach (var humanBody in eventArgs.added)
         {
             if (!skeletonTracker.TryGetValue(humanBody.trackableId, out humanBoneController))
             {
-                Debug.Log($"Adding a new skeleton [{humanBody.trackableId}].");
+                UnityEngine.Debug.Log($"Adding a new skeleton [{humanBody.trackableId}].");
                 var newSkeletonGO = Instantiate(skeletonPrefab, humanBody.transform);
+
+                if (jointTrackers == null)
+                {
+                    jointTrackers = newSkeletonGO.GetComponentsInChildren<JointTracker>();
+                    foreach(JointTracker jointTracker in jointTrackers)
+                    {
+                        SpeedLeft.text += $" Bone: {jointTracker.gameObject.transform.parent.name} Position: {jointTracker.gameObject.transform.position} \n";
+                        //SpeedLeft.text += $" Bone: {jointTracker.gameObject.transform.parent.name} Local Position: {jointTracker.gameObject.transform.localPosition} \n";
+                        //calculate speed
+                        
+                        //SpeedRigth.text += $" Bone: {jointTracker.gameObject.transform.parent.name} Local Position: {jointTracker.gameObject.transform.localPosition} \n";
+                    }
+                }
 
                 humanBoneController = newSkeletonGO.GetComponent<HumanBoneController>();
 
@@ -72,12 +111,6 @@ public class HumanBodyTracking : MonoBehaviour
 
             humanBoneController.InitializeSkeletonJoints();
             humanBoneController.ApplyBodyPose(humanBody, Vector3.zero);
-
-            HumanBodyTrackerUI.Instance.humanBodyText.text = $"{this.gameObject.name} {humanBody.name} Position: {humanBody.transform.position}\n" +
-            $"LocalPosition: {humanBody.transform.localPosition}";
-
-            HumanBodyTrackerUI.Instance.humanBoneControllerText.text = $"{this.gameObject.name} {humanBoneController.name} Position: {humanBoneController.transform.position}\n" +
-            $"LocalPosition: {humanBoneController.transform.localPosition}";
         }
 
         foreach (var humanBody in eventArgs.updated)
@@ -86,17 +119,64 @@ public class HumanBodyTracking : MonoBehaviour
             {
                 humanBoneController.ApplyBodyPose(humanBody, Vector3.zero);
             }
+            if (jointTrackers != null)
+            {
+                //get last jointTrackers positions
+                Vector3 lastLeftHandPosition = new Vector3(0, 0, 0);
+                Vector3 lastRightHandPosition = new Vector3(0, 0, 0);
 
-            HumanBodyTrackerUI.Instance.humanBodyText.text = $"{this.gameObject.name} {humanBody.name} Position: {humanBody.transform.position}\n" +
-            $"LocalPosition: {humanBody.transform.localPosition}";
+                foreach (JointTracker jointTracker in jointTrackers)
+                {
+                    if (jointTracker.gameObject.transform.parent.name.Equals("LeftHand"))
+                        lastLeftHandPosition = jointTracker.gameObject.transform.position;
 
-            HumanBodyTrackerUI.Instance.humanBoneControllerText.text = $"{this.gameObject.name} {humanBoneController.name} Position: {humanBoneController.transform.position}\n" +
-            $"LocalPosition: {humanBoneController.transform.localPosition}";
+                    if (jointTracker.gameObject.transform.parent.name.Equals("RightHand"))
+                        lastRightHandPosition = jointTracker.gameObject.transform.position;
+                }
+                //get new jointTrackers positions
+                Vector3 newLeftHandPosition = new Vector3(0, 0, 0);
+                Vector3 newRightHandPosition = new Vector3(0, 0, 0);
+
+                jointTrackers = humanBoneController.GetComponentsInChildren<JointTracker>();
+                foreach (JointTracker jointTracker in jointTrackers)
+                {
+                    SpeedLeft.text += $" Bone: {jointTracker.gameObject.transform.parent.name} Position: {jointTracker.gameObject.transform.position} \n";
+                    //SpeedLeft.text += $" Bone: {jointTracker.gameObject.transform.parent.name} Local Position: {jointTracker.gameObject.transform.localPosition} \n";
+
+                    if (jointTracker.gameObject.transform.parent.name.Equals("LeftHand"))
+                    {
+                        newLeftHandPosition = jointTracker.gameObject.transform.position;
+                        //The world space position of the transform
+                        /*
+                        newLeftHandPosition = jointTracker.gameObject.transform.position;
+                        SpeedLeft.text += $"X: {newLeftHandPosition.x}\n";
+                        SpeedLeft.text += $"Y: {newLeftHandPosition.y}\n";
+                        SpeedLeft.text += $"Z: {newLeftHandPosition.z}\n";
+                        //SpeedLeft.text += $"Velocidad(I): {getJointMovementSpeed(lastLeftHandPosition, newLeftHandPosition)}";*/
+                    }
+
+                    if (jointTracker.gameObject.transform.parent.name.Equals("RightHand"))
+                    {
+                        newRightHandPosition = jointTracker.gameObject.transform.position;
+                        /*SpeedRigth.text += $"X: {newRightHandPosition.x}\n";
+                        SpeedRigth.text += $"Y: {newRightHandPosition.y}\n";
+                        SpeedRigth.text += $"Z: {newRightHandPosition.z}\n";
+                        //SpeedRigth.text += $"Velocidad(D): {getJointMovementSpeed(lastRightHandPosition, newRightHandPosition)}";*/
+                    }
+                        
+                }
+
+                SpeedRigth.text += $"Resumen: {poseController.ResumePosition(jointTrackers)}\n";
+                SpeedRigth.text += $"Pausa: {poseController.PausePosition(jointTrackers)}\n";
+
+                var speed = getJointMovementSpeed(lastLeftHandPosition, newLeftHandPosition);
+                //SpeedRigth.text += $"Velocidad Izquierda: {speed}";
+            }
         }
 
         foreach (var humanBody in eventArgs.removed)
         {
-            Debug.Log($"Removing a skeleton [{humanBody.trackableId}].");
+            UnityEngine.Debug.Log($"Removing a skeleton [{humanBody.trackableId}].");
             if (skeletonTracker.TryGetValue(humanBody.trackableId, out humanBoneController))
             {
                 Destroy(humanBoneController.gameObject);
@@ -104,7 +184,32 @@ public class HumanBodyTracking : MonoBehaviour
             }
         }
 
-        HumanBodyTrackerUI.Instance.humanBodyTrackerText.text = $"{this.gameObject.name} Position: {this.gameObject.transform.position}\n" +
-            $"LocalPosition: {this.gameObject.transform.localPosition}";
+        //To get time execution
+        DateTime end = DateTime.Now;
+        TimeSpan ts = (end - start);
+        /*SpeedRigth.text += $"Tiempo Inicio: {start:MM/dd/yyyy hh:mm:ss.fff tt}\n";
+        SpeedRigth.text += $"Tiempo Fin: {end:MM/dd/yyyy hh:mm:ss.fff tt}\n";*/
+        TimeCounter += ts.TotalSeconds*100;
+
+        if (TimeCounter > 1)
+            TimeCounter = 0;
+        //SpeedLeft.text += $"Tiempo: {ts.TotalMilliseconds} ms\n";
+        //SpeedLeft.text += $"Tiempo Ejecutando: {TimeCounter} sec\n";
+       
+    }
+
+    double getJointMovementSpeed(Vector3 lastPosition, Vector3 newPostion)
+    {
+        //var speed = Mathf.Pow((int)(newPostion.x*10) - (int)(lastPosition.x*10), 2) + Mathf.Pow((int)(newPostion.y*10) - (int)(lastPosition.y*10), 2) + Mathf.Pow((int)(newPostion.z*10) - (int)(lastPosition.z*10), 2);
+        var lastx = (float) Math.Round(lastPosition.x * 10,2);
+        var lasty = (float)Math.Round(lastPosition.y * 10, 2);
+        var lastz = (float)Math.Round(lastPosition.z * 10, 2);
+        var newx = (float)Math.Round(newPostion.x * 10, 2); ;
+        var newy = (float)Math.Round(newPostion.y * 10, 2); ;
+        var newz = (float)Math.Round(newPostion.z * 10, 2); ;
+        //var distance = Math.Sqrt(Math.Pow(newx - lastx, 2)+ Math.Pow(newy - lasty, 2)+ Math.Pow(newz - lastz, 2));
+        var distance = newx - lastx;
+        //return Mathf.Sqrt(speed);
+        return distance;
     }
 }
